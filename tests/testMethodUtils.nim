818ff0fe299f438d8c01715020f187bbcc6c2833
Jake Leahy <jake@leahy.dev>
2022-06-27 22:04:33 +1000

    Cleaned tests

--- tests/testMethodUtils.nim
+++ tests/testMethodUtils.nim
@@ -1,12 +1,13 @@
 import jamp/methods
 import jamp/common
 import unittest
+import std/options
 import json
 
 # Test that the jmapMethod macro generates correct code
 
 test "Normal parameters":
-  check JsonNode.baseGet("1234", @["1", "2"]) == %* {
+  check Base.get("1234", @["1", "2"]) == %* {
     "accountId": "1234",
     "ids": @["1", "2"],
     "properties": @["id"]
@@ -29,8 +30,23 @@ test "addParam":
   data.addParam(foo)
   assert "#foo" in data
 
+test "Call with pass":
+  type
+    Foo = object
+  proc bar(_: typedesc[Foo], x, b: int, l: string) = 
+    check:
+      x == 9
+      b == 4
+      l == "foo"
+  let
+    x = 9
+    b = 4
+    l = "foo"
+  callWithPass(Foo, bar)
+
+
 test "Passing reference to previous result":
-  check JsonNode.baseGet("1234", ResultReference(
+  check Base.get("1234", ResultReference(
     resultOf: "5678",
     name: "Foo/get",
     path: "/test/*"
@@ -43,3 +59,11 @@ test "Passing reference to previous result":
     },
     "properties": @["id"]
   }
+
+test "Creating a new method":
+  type
+    Foo = object
+  proc get[Foo](x: int): Call[Option[string]] {.jmapMethod(Base).} =
+    result.needed = "bar"
+
+  check Foo.get("1234", 9).needed == "bar"
